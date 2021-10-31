70357a8519d20742241cf2fd639e4a5f8f9ba3a7
knolucn <knol@ucn.dk>
2021-10-31 21:13:50 +0100

    hello method

--- src/Test.java
+++ src/Test.java
@@ -3,4 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
 	}
+	
+	public static void hello(String name) {
+		System.out.println("Hello, " + name);
+	}
 }
