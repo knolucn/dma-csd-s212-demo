23985e3d79ea89b219731cb91c287be9fe80a0de
knolucn <knol@ucn.dk>
2021-10-31 21:26:16 +0100

    unfubah

--- src/Test.java
+++ src/Test.java
@@ -3,13 +3,13 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
 	}
-	
-<<<<<<< HEAD
-	public static void add(int a , int b) {
+
+	public static void add(int a, int b) {
 		System.out.println("Sum is: " + (a + b));
-=======
+
+	}
+
 	public static void hello(String name) {
 		System.out.println("Hello, " + name);
->>>>>>> branch 'main' of https://github.com/knolucn/dma-csd-s212-demo.git
 	}
 }
