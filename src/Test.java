7dc82176e8b86b216c0f54658381140d220f794a
knol <knol@ucn.dk>
2021-10-31 21:14:37 +0100

    plus method

--- src/Test.java
+++ src/Test.java
@@ -3,4 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
 	}
+	
+	public static void add(int a , int b) {
+		System.out.println("Sum is: " + (a + b));
+	}
 }
